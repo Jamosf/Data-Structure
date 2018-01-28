dcb2cdc5ab01e887824e3ce1fb03192de436ccad
fengshangren <271917994@qq.com>
2018-01-28 12:32:17 +0800

    2018-1-28

---
+++ 412-Fizz-Buzz/doc.go
@@ -0,0 +1,6 @@
+// 412-Fizz-Buzz project doc.go
+
+/*
+412-Fizz-Buzz document
+*/
+package main
---
+++ 412-Fizz-Buzz/main.go
@@ -0,0 +1,28 @@
+// 412-Fizz-Buzz project main.go
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+func fizzBuzz(n int) []string {
+	var r []string
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 != 0 {
+			r = append(r, "Fizz")
+		} else if i%3 != 0 && i%5 == 0 {
+			r = append(r, "Buzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			r = append(r, "FizzBuzz")
+		} else {
+			r = append(r, strconv.Itoa(i))
+		}
+	}
+	return r
+}
