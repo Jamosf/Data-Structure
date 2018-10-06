dd34c3917efb007ae504360666424a6781a48002
fengshangren <271917994@qq.com>
2018-10-06 19:38:10 +0800

    2018-10-6

---
+++ 437-Path-Sum-III/main.go
@@ -0,0 +1,30 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func dfs(root *TreeNode, sum int, count *int) {
+	if root == nil {
+		return
+	}
+	sum -= root.Val
+	if sum == 0 {
+		*count = *count + 1
+		return
+	}
+	dfs(root.Left, sum, count)
+	dfs(root.Right, sum, count)
+}
+
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	var count int
+	dfs(root, sum, &count)
+	return count + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+}
