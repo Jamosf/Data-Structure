85237ff43ea311bc0eb6ffd68a3fcfe6752fe9eb
Jamosf <271917994@qq.com>
2018-11-07 20:21:34 +0800

    2018-11-7

--- sort_practise/10-5main.cpp
+++ sort_practise/10-5main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int A[Max] = {0};
 
 struct Person{
+    string namesb;
     int score[Max];
+    int correctCount;
     int total;
 };
 
@@ -27,20 +29,73 @@ int main(){
     for(int i = 0; i < n;i++){
         cin >> A[i];
     }
-    Person *pl = (Person*)malloc(sizeof(Person)*p);
+    /*构建一个桶，用于存储person类*/
+    Person *pl = new Person[p];
+    for(int i = 0;i < p;i++){
+        for(int j = 0; j < n;j++){
+            pl[i].score[j] = -2;
+        }
+        pl[i].total = 0;
+        pl[i].correctCount = 0;
+    }
     string name;
     int number;
     int score;
     for(int i = 0; i < s;i++){
         cin >> name >> number >> score;
-        if(pl[str2int(name)].score[number] < score){
-            pl[str2int(name)].score[number] = score;
+        pl[str2int(name)-1].namesb = name;
+        if(pl[str2int(name)-1].score[number-1] < score){
+            pl[str2int(name)-1].score[number-1] = score;
+        }
+        if(score == -1) pl[str2int(name)-1].score[number-1] = 0;
+        if(score == A[number-1]){
+            pl[str2int(name)-1].correctCount++;
         }
     }
+    bool flag;
     for(int i = 0; i < p;i++){
+        flag = false;
         for(int j = 0;j < n;j++){
-            pl[i].total += pl[i].score[j];
+            if(pl[i].score[j] > 0 || pl[i].score[j] == -1){
+                pl[i].total += pl[i].score[j];
+                flag = true;
+            }
+        }
+        if(!flag)pl[i].total = -1;
+    }
+    Person tmp;
+    for(int i = 0; i < p-1;i++){
+        for(int j = 0;j < p-1-i;j++){
+            if(pl[j].total < pl[j+1].total){
+                tmp = pl[j];
+                pl[j] = pl[j+1];
+                pl[j+1] = tmp;
+            }else if(pl[j].total == pl[j+1].total){
+                if(pl[j].correctCount < pl[j+1].correctCount){
+                    tmp = pl[j];
+                    pl[j] = pl[j+1];
+                    pl[j+1] = tmp;
+                }
+            }
         }
     }
-    
+    int rank = 0;
+    for(int i = 0; i < p;i++){
+        if(pl[i].total >= 0){
+            if(i == 0 || (i >= 1 && pl[i].total != pl[i-1].total)){
+                rank += 1;
+            }
+            cout << rank << " " << pl[i].namesb << " " << pl[i].total;
+            for(int j = 0; j < n;j++){
+                if(pl[i].score[j] >= 0 || pl[i].score[j] == -1){
+                    cout << " " << pl[i].score[j];
+                }else{
+                    cout << " -";
+                }
+            }
+            cout << endl;
+        }
+    }
+    delete[]pl;
+    system("pause");
 }
