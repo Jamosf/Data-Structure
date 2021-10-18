7238844995f3506939a6420ed8535c047101a77d
Jamosf <Jamosf@163.com>
2021-10-18 09:28:12 +0800

    update

--- leetcode/2021-fight-for-job/1014_test.go
+++ leetcode/2021-fight-for-job/1014_test.go
@@ -46,3 +46,94 @@ func Test_fourSum(t *testing.T) {
 	fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))
 	fmt.Println(fourSum([]int{2, 2, 2, 2, 2}, 8))
 }
+
+// leetcode8
+func myAtoi(s string) int {
+	ss := strings.TrimLeft(s, " ")
+	ans := make([]byte, 0)
+	for i := range ss {
+		v := ss[i]
+		if v >= '0' && v <= '9' {
+			ans = append(ans, v)
+		}
+		if v == '-' || v == '+' {
+			if len(ans) == 0 {
+				ans = append(ans, v)
+			} else {
+				break
+			}
+		}
+		if (v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z') || v == '.' || v == ' ' {
+			break
+		}
+	}
+	var factor int
+	if len(ans) > 0 {
+		if ans[0] == '-' {
+			factor = -1
+			ans = ans[1:]
+		} else if ans[0] == '+' {
+			factor = 1
+			ans = ans[1:]
+		} else {
+			factor = 1
+		}
+	}
+	res := 0
+	t := 1
+	for i := range ans {
+		res += int(ans[len(ans)-i-1]-'0') * t
+		if factor == 1 && (res > math.MaxInt32 || t > math.MaxInt32) {
+			return math.MaxInt32
+		}
+		if factor == -1 && (res < math.MinInt32 || t < math.MinInt32) {
+			return math.MinInt32
+		}
+		t *= 10
+	}
+	r := factor * res
+	if r > math.MaxInt32 {
+		return math.MaxInt32
+	}
+	if r < math.MinInt32 {
+		return math.MinInt32
+	}
+	return r
+}
+
+func Test_myAtoi(t *testing.T) {
+	fmt.Println(myAtoi("10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000522545459"))
+}
+
+// leetcode8 优化解法
+func myAtoi1(s string) int {
+	abs, sign, i, n := 0, 1, 0, len(s)
+	//丢弃无用的前导空格
+	for i < n && s[i] == ' ' {
+		i++
+	}
+	//标记正负号
+	if i < n {
+		if s[i] == '-' {
+			sign = -1
+			i++
+		} else if s[i] == '+' {
+			sign = 1
+			i++
+		}
+	}
+	for i < n && s[i] >= '0' && s[i] <= '9' {
+		abs = 10*abs + int(s[i]-'0')  //字节 byte '0' == 48
+		if sign*abs < math.MinInt32 { //整数超过 32 位有符号整数范围
+			return math.MinInt32
+		} else if sign*abs > math.MaxInt32 {
+			return math.MaxInt32
+		}
+		i++
+	}
+	return sign * abs
+}
+
+func Test_forsum(t *testing.T) {
+	fmt.Println(fourSum([]int{2, 2, 2, 2, 2}, 8))
+}
