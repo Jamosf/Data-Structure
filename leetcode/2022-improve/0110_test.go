84f798721a3eebc163899a9225887f875688003a
Jamosf <Jamosf@163.com>
2022-01-12 18:06:15 +0800

    日常刷题

---
+++ leetcode/2022-improve/0110_test.go
@@ -0,0 +1,149 @@
+package _022_improve
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+// tag-[回溯]
+// leetcode306:累加数
+func isAdditiveNumber(num string) bool {
+	n := len(num)
+	memo := make([][]int, 40)
+	for i := range memo{
+		memo[i] = make([]int, 40)
+		for j := range memo[i]{
+			memo[i][j] = -1
+		}
+	}
+	var tmp []int
+	var dfs func(i int) bool
+	dfs = func(i int) bool{
+		if size := len(tmp); size > 2 && tmp[size-2] + tmp[size-3] != tmp[size-1]{
+			return false
+		}
+		if i == n && len(tmp) > 2{
+			return true
+		}
+		for p := i+1; p <= n; p++{
+			if p-i >1 && num[i] == '0'{
+				continue
+			}
+			if memo[i][p] == -1{
+				memo[i][p], _ = strconv.Atoi(num[i:p])
+			}
+			tmp = append(tmp, memo[i][p])
+			if dfs(p){
+				return true
+			}
+			tmp = tmp[:len(tmp)-1]
+		}
+		return false
+	}
+	return dfs(0)
+}
+
+func Test_isAdditiveNumber(t *testing.T){
+	fmt.Println(isAdditiveNumber("011"))
+	fmt.Println(isAdditiveNumber("1120358"))
+	fmt.Println(isAdditiveNumber("199100199"))
+}
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+// tag-[链表]
+// leetcode24: 两两交换链表中的节点
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil{
+		return nil
+	}
+	p := head
+	for i := 0; i < 2 ; i++{
+		if head == nil{
+			return p
+		}
+		head = head.Next
+	}
+	newNode := reverse(p, p.Next)
+	p.Next = swapPairs(head)
+	return newNode
+}
+
+func reverse(p, q *ListNode) *ListNode{
+	var pre *ListNode
+	cur := p
+	for cur != q{
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
+// tag-[滑动窗口]
+// leetcode30: 串联所有单词的子串
+func findSubstring(s string, words []string) []int {
+	size, step := len(s), len(words[0])
+	m := make(map[string]int)
+	n := make(map[string]int)
+	for i := range words{
+		m[words[i]]++
+	}
+	idx := 0
+	var ans []int
+	start := 0
+	for idx <= size-step{
+		if _, ok := m[s[idx:idx+step]]; !ok{
+			idx++
+			start = idx
+			n = make(map[string]int)
+			continue
+		}
+		n[s[idx:idx+step]]++
+		idx += step
+		for moreThan(m, n){
+			n[s[start:start+step]]--
+			start += step
+		}
+		if reflect.DeepEqual(m, n){
+			ans = append(ans, start)
+		}
+		for i := 1; i < step; i++ {
+			p := start+i
+			k := make(map[string]int)
+			for p <= idx && p+step <= size{
+				k[s[p:p+step]]++
+				p += step
+			}
+			if reflect.DeepEqual(m, k){
+				ans = append(ans, start+i)
+			}
+		}
+	}
+	return ans
+}
+
+func moreThan(m, n map[string]int) bool{
+	for k, v := range m{
+		if n[k] > v{
+			return true
+		}
+	}
+	return false
+}
+
+func Test_findSubstring(t *testing.T){
+	// fmt.Println(findSubstring("barfoothefoobarman", []string{"foo", "bar"}))
+	// fmt.Println(findSubstring("wordgoodgoodgoodbestword", []string{"word","good","best","word"}))
+	// fmt.Println(findSubstring("abarfoobfoobarthefoobarman", []string{"bar","foo","the"}))
+	// fmt.Println(findSubstring("wordgoodgoodgoodbestword", []string{"word","good","best","good"}))
+	// fmt.Println(findSubstring("ababababababab", []string{"aba","bab"}))
+	// fmt.Println(findSubstring("aaaaaaaaaaaaaa", []string{"aa","aa"}))
+	fmt.Println(findSubstring("ababaab", []string{"ab","ba","ba"}))
+}
---
+++ leetcode/2022-improve/0111_test.go
@@ -0,0 +1,82 @@
+package _022_improve
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+// tag-[深度优先搜索]
+// leetcode1036: 逃离大迷宫
+func isEscapePossible(blocked [][]int, source []int, target []int) bool {
+	b := make(map[string]bool)
+	for _, block := range blocked{
+		b[strconv.Itoa(block[0])+"->"+strconv.Itoa(block[1])] = true
+	}
+	inf := int(1e6)
+	var dfs func(x, y int, s, t []int, vis map[string]bool) bool
+	dfs = func(x, y int, s, t []int, vis map[string]bool) bool {
+		if x >= inf || x < 0 || y >= inf || y < 0{
+			return false
+		}
+		key := strconv.Itoa(x)+"->"+strconv.Itoa(y)
+		if vis[key] || b[key] {
+			return false
+		}
+		if (x == t[0] && y == t[1]) || abs(s[0]-x) + abs(s[1]-y) > 200{ // 曼哈顿距离大于200可以绕着走
+			return true
+		}
+		vis[key] = true
+		return dfs(x+1, y, s, t, vis) || dfs(x-1, y, s, t, vis) || dfs(x, y+1, s, t, vis) || dfs(x, y-1, s, t, vis)
+	}
+	return dfs(source[0], source[1], source, target, map[string]bool{}) && dfs(target[0], target[1], target,  source, map[string]bool{})
+}
+
+func abs(x int) int{
+	if x < 0{
+		return -x
+	}
+	return x
+}
+
+func Test_e(t *testing.T){
+	fmt.Println(int(1e6))
+}
+
+// tag-[广度优先搜索]
+// leetcode1036: 逃离大迷宫
+// 思路：计算障碍物能包围的最大面积，如果从source和target能遍历的值大于最大面积，则说明一定没有被包围，则可以连通
+func isEscapePossible_(blocked [][]int, source []int, target []int) bool {
+	n := len(blocked)
+	max := n*(n-1)/2
+	b := make(map[string]bool)
+	for _, block := range blocked{
+		b[strconv.Itoa(block[0])+"->"+strconv.Itoa(block[1])] = true
+	}
+	dirs := [4][2]int{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+	bfs := func(s, t []int) bool {
+		vis := make(map[string]bool)
+		queue := make([][2]int, 0, max)
+		queue = append(queue, [2]int{s[0], s[1]})
+		vis[strconv.Itoa(s[0])+"->"+strconv.Itoa(s[1])] = true
+		cnt := 0
+		for len(queue) > 0{
+			v := queue[0]
+			queue = queue[1:]
+			cnt++
+			if (v[0] == t[0] && v[1] == t[1]) || cnt > max{
+				return true
+			}
+			for i := 0; i < 4; i++{
+				x, y := v[0] + dirs[i][0], v[1] + dirs[i][1]
+				key := strconv.Itoa(x)+"->"+strconv.Itoa(y)
+				if x >= 0 && x < 1e6 && y >= 0 && y < 1e6 && !vis[key] && !b[key]{
+					queue = append(queue, [2]int{x, y})
+					vis[key] = true
+				}
+			}
+		}
+		return false
+	}
+	return bfs(source, target) && bfs(target, source)
+}
