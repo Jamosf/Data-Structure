a56bd31e787b55cf5fc74704f6bb07c1e697445d
Jamosf <271917994@qq.com>
2022-01-15 19:43:42 +0800

    updtae

---
+++ leetcode/2022-improve/0108_test.go
@@ -0,0 +1,15 @@
+package _022_improve
+
+// tag-[二进制]
+// leetcode89: 格雷编码
+func grayCode(n int) []int {
+	head := 1
+	ans := []int{0}
+	for i := 0; i < n; i++ {
+		for j := len(ans) - 1; j >= 0; j-- {
+			ans = append(ans, head+ans[j])
+		}
+		head <<= 1
+	}
+	return ans
+}
---
+++ leetcode/2022-improve/0109_test.go
@@ -0,0 +1,23 @@
+package _022_improve
+
+// tag-[数组]
+// leetcode1629: 按键持续时间最长的键
+func slowestKey(releaseTimes []int, keysPressed string) byte {
+	n := len(releaseTimes)
+	tmp := append([]int{0}, releaseTimes...)
+	var count [26]int
+	for i := 1; i <= n; i++ {
+		diff := tmp[i] - tmp[i-1]
+		v := keysPressed[i-1] - 'a'
+		count[v] = max(count[v], diff)
+	}
+	maxn := count[0]
+	ans := byte('a')
+	for i := range count {
+		if count[i] >= maxn {
+			ans = byte(i + 'a')
+			maxn = count[i]
+		}
+	}
+	return ans
+}
