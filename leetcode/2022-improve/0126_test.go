6e347b255e883082b7c76d4ef5dfd47666113d2f
Jamosf <271917994@qq.com>
2022-01-26 21:43:02 +0800

    update

---
+++ leetcode/2022-improve/0126_test.go
@@ -0,0 +1,19 @@
+package _022_improve
+
+// tag-[数学]
+// 每日一题
+// leetcode2013: 检测正方形
+type DetectSquares struct {
+}
+
+func ConstructorDetectSquares() DetectSquares {
+
+}
+
+func (this *DetectSquares) Add(point []int) {
+
+}
+
+func (this *DetectSquares) Count(point []int) int {
+
+}
